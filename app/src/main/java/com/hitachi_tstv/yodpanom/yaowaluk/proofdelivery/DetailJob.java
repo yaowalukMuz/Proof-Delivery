cb7cb26589316c3ec3951281711b63d827a8c7fe
yaowamuz <yaowamuz.c@gmail.com>
2016-10-11 12:40:24 +0700

    Design UI

---
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/DetailJob.java
@@ -0,0 +1,13 @@
+package com.hitachi_tstv.yodpanom.yaowaluk.proofdelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_job);
+    }
+}
---
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/JobListView.java
@@ -0,0 +1,13 @@
+package com.hitachi_tstv.yodpanom.yaowaluk.proofdelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JobListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_list_view);
+    }
+}

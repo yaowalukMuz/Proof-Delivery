94530974036084099e22dd31b9514c1921f76b75
yaowamuz <yaowamuz.c@gmail.com>
2016-10-11 17:09:31 +0700

    end 11Oct _get intent

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/ServiceActivity.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/ServiceActivity.java
@@ -2,12 +2,41 @@ package com.hitachi_tstv.yodpanom.yaowaluk.proofdelivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView nameDriverTextView,idDriverTextView;
+    private Button jobListButton, closeButton;
+    private ListView  listView;
+    private String[] loginStrings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+
+        //BindWidget
+        nameDriverTextView = (TextView) findViewById(R.id.textView2);
+        idDriverTextView = (TextView) findViewById(R.id.textView4);
+        jobListButton = (Button) findViewById(R.id.button3);
+        closeButton = (Button) findViewById(R.id.button2);
+        listView = (ListView) findViewById(R.id.listJob);
+
+
+        // get value from Inten
+
+        loginStrings = getIntent().getStringArrayExtra("Login");
+
+        //Show name
+        nameDriverTextView.setText(loginStrings[1]);
+
+
+
+    }   //Main method
+}   //Main  Class

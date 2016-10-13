d5727082b84d5de90b81cfe5c3d254b18308ad4c
yaowamuz <yaowamuz.c@gmail.com>
2016-10-13 16:38:41 +0700

    drawing success--13Oct

--- app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/DegitalSignatureActivity.java
+++ app/src/main/java/com/hitachi_tstv/yodpanom/yaowaluk/proofdelivery/DegitalSignatureActivity.java
@@ -1,6 +1,7 @@
 package com.hitachi_tstv.yodpanom.yaowaluk.proofdelivery;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -22,9 +23,10 @@ public class DegitalSignatureActivity extends AppCompatActivity {
     private LinearLayout mContent;
     private EditText fullNameEditText;
     private Button saveButton,clearButton;
-    private signature mSignature;
+   // private signature mSignature;
     private View view;
-
+    DrawingView dv;
+    private Paint mPaint;
 
 
     @Override
@@ -39,16 +41,34 @@ public class DegitalSignatureActivity extends AppCompatActivity {
         saveButton.setEnabled(false);
         //
         //mContent.addView();
-        mSignature = new signature(getApplicationContext(), null);
-        mSignature.setBackgroundColor(Color.WHITE);
 
-        mContent.addView(mSignature, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        dv = new DrawingView(this);
+       // setContentView(dv);
+        mContent.addView(dv, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         saveButton.setEnabled(false);
-        view = mContent;
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStrokeWidth(6);
+
+
+
+
+      /*
+        dv = new DrawingView(getApplicationContext());
+        dv.setBackgroundColor(Color.WHITE);
+
+        mContent.addView(dv, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        saveButton.setEnabled(false);
+        view = mContent;*/
 
     }// Main Method
 
-    public class signature extends View{
+   /* public class signature extends View{
 
         private static final float STROKE_WIDTH = 5f;
         private static final float HALF_STROKE_WIDTH = STROKE_WIDTH/2;
@@ -140,7 +160,107 @@ public class DegitalSignatureActivity extends AppCompatActivity {
             dirtyRect.bottom = Math.max(lastTouchY, eventY);
         }
 
-    }
+    }*/
+
+
+    public class DrawingView extends View{
+        public int width;
+        public  int height;
+        private Bitmap mBitmap;
+        private Canvas mCanvas;
+        private Path mPath;
+        private Paint mBitmapPaint;
+        Context context;
+        private Paint circlePaint;
+        private Path circlePath;
+
+        public DrawingView(Context context) {
+            super(context);
+            context = context;
+            mPath = new Path();
+            mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+            circlePaint = new Paint();
+            circlePath = new Path();
+            circlePaint.setAntiAlias(true);
+            circlePaint.setColor(Color.BLACK);
+            circlePaint.setStyle(Paint.Style.STROKE);
+            circlePaint.setStrokeJoin(Paint.Join.MITER);
+            circlePaint.setStrokeWidth(4f);
+
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            super.onSizeChanged(w, h, oldw, oldh);
+
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+            mCanvas = new Canvas(mBitmap);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            canvas.drawBitmap(mBitmap, 0, 0,mBitmapPaint);
+            canvas.drawPath(mPath, mPaint);
+            canvas.drawPath(circlePath, circlePaint);
+        }
+
+        private float mX,mY;
+        private static final float TOUCH_TOLERANCE = 4;
+
+
+        private void touch_start(float x,float y){
+            mPath.reset();
+            mPath.moveTo(x, y);
+            mX = x;
+            mY = y;
+
+        }
+
+        private void touch_move(float x ,float y){
+            float dx = Math.abs(x - mX);
+            float dy = Math.abs(y -mY);
+            if (dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+                mPath.quadTo(mX, mY, (x + mX)/2, (y +mY)/2);
+                mX = x;
+                mY = y;
+
+                circlePath.reset();
+                circlePath.addCircle(mX, mY, 30, Path.Direction.CW);
+            }
+        }
+
+        private void touch_up(){
+            mPath.lineTo(mX, mY);
+            circlePath.reset();
+            // commit the path to our offscreen
+            mCanvas.drawPath(mPath,  mPaint);
+            // kill this so we don't double draw
+            mPath.reset();
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            float x = event.getX();
+            float y = event.getY();
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN :
+                    touch_start(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_MOVE :
+                    touch_move(x, y);
+                    invalidate();
+                    break;
+                case MotionEvent.ACTION_UP :
+                    touch_up();
+                    invalidate();
+                    break;
+            }
+            return true;
+        }
+    }//Class DrawingView
 
 
 }//Main Class
